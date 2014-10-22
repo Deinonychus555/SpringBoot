c03aa32ccd0f5a7619a968fd3bb283a7dccddffc
Echeverrias <juanan@Deinonychus>
2014-10-22 21:13:43 +0200

    Añadido:
    Validating Form Input

--- initial/src/main/java/hello/Person.java
+++ initial/src/main/java/hello/Person.java
@@ -6,15 +6,27 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+// Validating Form Input 
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+
 @Entity
 public class Person {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
-
+        
+        @Size(min=2, max=30)
 	private String firstName;
+        
 	private String lastName;
+        
+        @NotNull
+        @Min(18)
+        private Integer age;
 
 	public String getFirstName() {
 		return firstName;
@@ -31,4 +43,16 @@ public class Person {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+        
+        public Integer getAge() {
+            return age;
+        }
+        
+        public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Person(First name: " + this.firstName + "Last name: " + this.lastName + ", Age: " + this.age + ")";
+    }
 }
